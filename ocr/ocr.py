93960ecb6f4c6c86779c604440163fa370b6c781
Frederick Li (HK/ED) <yhli@tfprod.taifook.com>
2017-07-07 18:25:31 +0800

    20170707

---
+++ ocr/ocr.py
@@ -0,0 +1,31 @@
+from wand.image import Image
+from PIL import Image as PI
+import pyocr
+import pyocr.builders
+import io
+
+tool = pyocr.get_available_tools()[0]
+lang = tool.get_available_languages()[2]
+
+req_image = []
+
+image_pdf = Image(filename="./Notification Letter and Information Request-Haitong.pdf", resolution=300)
+image_jpeg = image_pdf.convert('jpeg')
+
+for img in image_jpeg.sequence:
+    img_page = Image(image=img)
+    req_image.append(img_page.make_blob('jpeg'))
+
+final_text = []
+# if True:
+for img in req_image: 
+    txt = tool.image_to_string(
+        PI.open(io.BytesIO(img)),
+        # PI.open("sample2.png"),
+        lang=lang,
+        builder=pyocr.builders.TextBuilder()
+    )
+    final_text.append(txt)
+
+for text in final_text:
+    print (text)
