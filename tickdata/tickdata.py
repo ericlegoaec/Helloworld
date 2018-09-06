15a8f8d7142396c7411c5dc478f0ce6201067c50
frederick623 <frederick623@gmail.com>
2018-09-07 00:15:07 +0800

    20180907

--- tickdata/tickdata.py
+++ tickdata/tickdata.py
@@ -1,39 +1,77 @@
 import os
 import sys
 import csv
+import re
+import sqlite3
 import requests
+import datetime
+import glob
+import pandas as pd
+import numpy as np
 
 CONST_DIC = {
-	"dw_list_file": "~/Projects/tmp/CFBC_20180622/dwFullList_20180622.csv",
-	"td30_file": "~/Projects/tmp/CFBC_20180622/MC30_AllFB_20180622.csv",
+	"sqlite_file": "/Users/frederickli/Projects/tmp/tickdata.db",
+	"dw_list_file": "/Users/frederickli/Projects/tmp/CFBC_20180622/dwFullList_20180622.csv",
+	"td30_file": "/Users/frederickli/Projects/tmp/CFBC_20180622/MC30_AllFB_20180622.csv",
+	"tmp_file": "/Users/frederickli/Projects/tmp/CFBC_20180622/tmp.csv",
 }
 
-HEADER = "ticktime,tradetype,ticker,"
+HEADER = "ordertime,msgtype,security,tradeid,orderid,price,quantity,trdtype,ticktime,side,ordertype,orderboookposition,aggshare" #+ (','.join(["arb_" + str(x) for x in range(13,18)]))
 
-def csv_to_arr(csv_file, start=0, end=0, has_header=True, delim=',', encoding='utf-8', ignore_cols=[]):
+def timer(func):
+	def wrapper(*args, **kwargs):
+		t1 = datetime.datetime.now()
+		func(*args, **kwargs)
+		t2 = datetime.datetime.now()
+		print( "Function %s Time it took to run the function: %s" % (func.__name__, str(t2 - t1)) )
+	return wrapper
+	
+def question_marks(st):
+	question_marks = '?'
+	for i in range(0, len(st.split(','))-1):
+		question_marks = question_marks + ",?"
+	return question_marks
+
+def db_cur(source = ":memory:"):
+	# sqlite3.register_adapter(decimal.Decimal, adapt_decimal)
+	# sqlite3.register_converter("DECTEXT", convert_decimal)
+	conn = sqlite3.connect(source, detect_types=sqlite3.PARSE_DECLTYPES)
+	conn.row_factory = sqlite3.Row
+	cur = conn.cursor()
+
+	return conn, cur
+
+def create_tbl(cur, tbl_name, header, arr = None, index_arr = None):
+	cur.execute("""select count(*) FROM sqlite_master WHERE type='table' AND name = '%s' """ % (tbl_name))
+	tbl_exists = cur.fetchone() 
+	if tbl_exists[0] == 0:
+		cur.execute("CREATE TABLE " + tbl_name + " (" + header + " );")
+		if index_arr is not None:
+			for index in index_arr:
+				cur.execute("CREATE INDEX " + tbl_name + "_" + index + " ON " + tbl_name + " (" + index + ");")
+	else:
+		cur.execute("""Delete From %s""" % (tbl_name))
+		
+	if arr is not None and len(arr) > 0:
+		cur.executemany("INSERT INTO " + tbl_name + "( " + header + " ) VALUES ("+question_marks(header)+")", arr)
+	return 
+
+def csv_to_arr(csv_file, has_header=True, delim=',', rows=None, cols=None ):
 	arr = []
 	reader = []
 
-	if "http" in csv_file:
-		response = requests.get(csv_file)
-		text = response.content.decode(encoding)
-	else:
-		text = open(csv_file, 'rU')
+	text = open(csv_file, 'rU')
 
 	reader = csv.reader(text, delimiter=delim)
 
 	arr = list(reader)
+	arr = np.array(arr)
 
-	if end != 0:
-		arr = arr[start:end]
-	else:
-		arr = arr[start:]
+	if rows != None:
+		arr = arr[rows,:]
 
-	# arr = list(zip(*arr))
-	# arr = [x for x in arr if any(x)]
-	# for col in ignore_cols:
-	# 	del arr[col]
-	# arr = list(zip(*arr))
+	if cols != None:
+		arr = arr[:,cols]
 	
 	header = ""
 
@@ -45,6 +83,24 @@ def csv_to_arr(csv_file, start=0, end=0, has_header=True, delim=',', encoding='u
 
 	return
 
+
+@timer
+def make_tmp():
+	fulllist_df = pd.read_csv(max(glob.iglob(CONST_DIC["dw_list_file"])), encoding='utf-16', index_col=False, skip_blank_lines=True, error_bad_lines=False, sep='\t', skiprows=1)
+	fulllist_df = fulllist_df.iloc[0:-3]
+	fulllist_df.columns = [re.sub(r"[\^\*\.#/\$%\"\(\)& :]", "", c) for c in fulllist_df.columns]
+	fulllist_df = fulllist_df.rename(columns={"DWCode":"security"})
+
+	arr = csv_to_arr(CONST_DIC["td30_file"], has_header=False, cols=list(range(0,13)) )
+	df = pd.DataFrame(data=arr, columns=HEADER.split(','))
+
+	df = pd.merge(df, fulllist_df, on=["security"], how="left")
+	print (df.iloc[1])
+	lite_df = df.loc[(df.UL == "00700") | (df.security == "700")][HEADER.split(',')]
+	lite_df = lite_df.iloc[1:100000]
+	lite_df.to_csv(CONST_DIC["tmp_file"], index=False, header=False)
+
+	return
 def main():
 
 	return
--- vimeo_dl.py
+++ vimeo_dl.py
@@ -1,37 +1,35 @@
-
+import os
+import re
 import requests
 import base64
+from moviepy.editor import VideoFileClip
 from tqdm import tqdm
 
 login_info = {"username":"venuschw","password":"Venus2834"}
 
-main_url = "https://www.billyshowell.com"
-drawing_url = "https://www.billyshowell.com/membership/my-video-tutorials/34-my-tutorials/paid-tutorials/suitable-for-all/670-strawberry-leaf-tutorial"
 
-def main():
-    res = requests.post(main_url, login_info)
-    session = requests.Session()
+# def main():
+#     res = requests.post(main_url, login_info)
+#     session = requests.Session()
 
-    cookie = res.headers["Set-Cookie"]
+#     cookie = res.headers["Set-Cookie"]
 
-    resp = requests.get(drawing_url)
-    return
+#     resp = requests.get(drawing_url)
+#     return
 
-def dl():
-    master_json_url = 'https://50skyfiregce-vimeo.akamaized.net/exp=1534104796~acl=%2F277247049%2F%2A~hmac=b9c4eee6b9ffb13c2ffe48a1280a416d219d903d41780b1d4fdf90808203e1e7/277247049/sep/video/1036621174,1036621179,1036621173,1036621172/master.json?base64_init=1'
+def video_dl(master_json_url):
     base_url = master_json_url[:master_json_url.rfind('/', 0, -26) + 1]
 
     resp = requests.get(master_json_url)
-    print (resp.text)
     content = resp.json()
 
     heights = [(i, d['height']) for (i, d) in enumerate(content['video'])]
+    print (heights)
     idx, _ = max(heights, key=lambda t: t[1])
     video = content['video'][idx]
     video_base_url = base_url + video['base_url']
     print ('base url:', video_base_url)
-
-    filename = 'video_%d.mp4' % video['id']
+    filename = 'video_%s.mp4' % video['id']
     print ('saving to %s' % filename)
 
     video_file = open(filename, 'wb')
@@ -52,6 +50,56 @@ def dl():
 
     video_file.flush()
     video_file.close()
+    return filename
+
+def audio_dl(master_json_url):
+    base_url = master_json_url[:master_json_url.rfind('/', 0, -26) + 1]
+
+    resp = requests.get(master_json_url)
+    content = resp.json()
+
+    audio = content['audio'][0]
+    audio_base_url = base_url + audio['base_url']
+    audio_base_url = re.sub(r"\/[a-z]*\/\.\.", "", audio_base_url)
+    print ('base url:', audio_base_url)
+    filename = 'audio_%s.mp4' % audio['id']
+    print ('saving to %s' % filename)
+
+    audio_file = open(filename, 'wb')
+
+    init_segment = base64.b64decode(audio['init_segment'])
+    audio_file.write(init_segment)
+
+    for segment in tqdm(audio['segments']):
+        segment_url = audio_base_url + segment['url']
+        resp = requests.get(segment_url, stream=True)
+        if resp.status_code != 200:
+            print ('not 200!')
+            print (resp)
+            print (segment_url)
+            break
+        for chunk in resp:
+            audio_file.write(chunk)
+
+    audio_file.flush()
+    audio_file.close()
+    return filename
+
+def combine(video_file, audio_file):
+    video_clip = VideoFileClip(video_file)
+    video_clip.write_videofile(os.path.join("/Users/frederickli/Downloads", os.path.basename(video_file.replace("video_", ""))), audio=audio_file)
+    os.remove(video_file)
+    os.remove(audio_file)
     return
 
-main()
+def main():
+    url = input("Enter the master.json: ")
+    video_file = video_dl(url)
+    audio_file = audio_dl(url)
+    # video_file = r"/Users/frederickli/Projects/video_1055442259.mp4"
+    # audio_file = r"/Users/frederickli/Projects/audio_1055442245.mp4"
+    combine(video_file, audio_file)
+    return
+
+if __name__ == "__main__":
+    main()
